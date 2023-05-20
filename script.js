66824d1b3a8fec772167c88185a96b6e5d65a1df
Jblaze <102456506+xinsideoutx@users.noreply.github.com>
2023-05-20 23:46:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ const navList = document.querySelector(".nav__list");
 menuIcon.addEventListener("click", function () {
   navList.classList.toggle("open");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollRev = ScrollReveal({
+    origin: "top",
+    distance: "85px",
+    duration: 2300,
+    reset: true,
+  });
+
+  scrollRev.reveal(".home__text", { delay: 200 });
+  scrollRev.reveal(".home__img", { delay: 300 });
+
+  scrollRev.reveal(".special", { delay: 400 });
+
+  scrollRev.reveal(".about img", {});
+  scrollRev.reveal(".about__text", { delay: 150 });
+
+  scrollRev.reveal(".heading-primary", {});
+  scrollRev.reveal(".row-btn, .shop-box", { delay: 200 });
+
+  scrollRev.reveal(".reviews__content, .center-text-reviews", { delay: 200 });
+
+  scrollRev.reveal(".contact__content", { delay: 200 });
+});
